55da6183b031f302feb59570d3f9ba46fc1040e1
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2026-02-25 23:04:25 +0530

    Implement string reversal without string functions
    Added a new implementation to reverse a string without using string functions.

--- Strings/reverse_a_string.c
+++ Strings/reverse_a_string.c
@@ -1,3 +1,4 @@
+// using string function
 #include<stdio.h>
 #include<string.h>
 
@@ -12,4 +13,32 @@ int main(){
     for(int i = len-1 ; i >= 0 ; i--){
         printf("%c",str[i]);
     }
+}
+
+
+
+// without using string functions 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[50];
+    int start = 0,end;
+    int temp;
+    
+    printf("Enter the string \n");
+    fgets(str,sizeof(str),stdin);
+    
+    end = strlen(str)-1;
+    
+    while(start<end){
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        
+        start++;
+        end--;
+    }
+    printf("Reverse String is %s",str);
+    return 0;
 }
