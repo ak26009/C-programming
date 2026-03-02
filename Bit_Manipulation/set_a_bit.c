3350c1a48c159dfcb68514af20337c51ba3e0dc2
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2026-03-03 00:50:32 +0530

    Add main function to set a bit in an integer

--- Bit_Manipulation/set_a_bit.c
+++ Bit_Manipulation/set_a_bit.c
@@ -8,3 +8,20 @@ int main() {
 
     printf("Result = %d\n", num);
 }
+
+
+// From User
+#include <stdio.h>
+
+int main() {
+    int num , pos;
+    
+    printf("Enter No. ");
+    scanf("%d",&num);
+    
+    printf("Enter Position  ");
+    scanf("%d",&pos);
+    
+    num = num | (1<<pos);
+    printf("Result = %d\n", num);
+}
